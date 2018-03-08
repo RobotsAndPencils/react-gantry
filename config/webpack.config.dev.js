37b8dd2f1d86077b7a2fc93afa47f636c14731fd
Andrew Krigline <andrew.krigline@robotsandpencils.com>
2018-03-08 17:54:15 -0500

    Added scss loaders and some comments about them

--- config/webpack.config.dev.js
+++ config/webpack.config.dev.js
@@ -7,6 +7,35 @@ module.exports = {
         test: /\.js$/,
         exclude: /node_modules/,
         loader: 'babel-loader'
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          { // Finally, after all our scss -> autoprefixer -> css happens, we have to inject it into our js bundle in a <style> tag (for development only)
+            loader: 'style-loader'
+          },
+          { // Enables CSS Modules and handles css's `@import` and `url()`s as if they were js imports (rewriting the url in the final css bundle)
+            loader: 'css-loader',
+            options: {
+              importLoaders: 2 // This will make both `sass-loader` and `postcss-loader` process any @imported css files before css-loader gets to them
+            }
+          },
+          { // Autoprefixer has to be loaded in through the postcss-loader
+            loader: 'postcss-loader',
+            options: {
+              sourceMap: true, // updates the sourcemap from the previous step (sass-loader)
+              plugins: () => [
+                require('autoprefixer')
+              ]
+            }
+          },
+          { // Wraps node-sass to compile scss files into css for the next step (postcss-loader)
+            loader: 'sass-loader',
+            options: {
+              sourceMap: true
+            }
+          }
+        ]
       }
     ]
   },
