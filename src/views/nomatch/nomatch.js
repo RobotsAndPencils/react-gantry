094d10ddd2e4ee337d07acfc0d361fb6719938ea
Stuart Tannehill <stuart.tannehill@robotsandpencils.com>
2018-03-26 16:57:17 -0400

    bug fix for nomatch

--- src/views/nomatch/nomatch.js
+++ src/views/nomatch/nomatch.js
@@ -1,32 +1,20 @@
 import React from 'react'
 import {
-  BrowserRouter as Router,
-  Route,
-  Link,
-  Switch
+  Link
 } from 'react-router-dom'
-import HomeRoute from '../home/home'
-import AboutView from '../about/about'
 
 const NoMatch = () => (
-  <Router>
-    <div>
-      <h1>404 - We can't find that page.</h1>
-      <ul>
-        <li>
-          <Link to='/'>Home</Link>
-        </li>
-        <li>
-          <Link to='/about'>About page</Link>
-        </li>
-      </ul>
-      <Switch>
-        <Route path='/' exact component={HomeRoute} />
-        <Route path='/about' component={AboutView} />
-        <Route component={NoMatch} />
-      </Switch>
-    </div>
-  </Router>
+  <div>
+    <h1>404 - We can't find that page.</h1>
+    <ul>
+      <li>
+        <Link to='/'>Home</Link>
+      </li>
+      <li>
+        <Link to='/about'>About page</Link>
+      </li>
+    </ul>
+  </div>
 )
 
 export default NoMatch
